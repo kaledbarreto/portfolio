b328d357f86c71f411e33326446952f6af05ee86
Kaled Barreto <kaledbarreto@gmail.com>
2022-07-13 14:00:25 -0300

    feat: Finishing hero section

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,9 +1,39 @@
-import { Container } from './styles';
+import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
+import placeholder from '../../assets/pessoa.webp';
 
 export function HomeHero() {
   return (
     <Container>
-      <h1>HomeHero</h1>
+      <img src={placeholder} alt="Hero" />
+      <div>
+        <TextContainer>
+          <h1>Olá</h1>
+          <h2>Me chamo Kaled</h2>
+        </TextContainer>
+        <InfosContainer>
+          <CodeItem>
+            <span className="comment">//Minha apresentação</span>
+            <span className="purple">Infos</span> {'\u007B'}
+            <div>
+              Idade: <span className="blue"> 20 </span>,
+            </div>
+            <div>
+              Cidade: <span className="blue">Lauro de Freitas, BA</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+          <CodeItem>
+            <span className="purple">Cargo</span> {'\u007B'}
+            <div>
+              Função: <span className="blue">Dev Full-stack</span>,
+            </div>
+            <div>
+              Situação: <span className="blue">Open to work 🚀​</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+        </InfosContainer>
+      </div>
     </Container>
   );
 }
